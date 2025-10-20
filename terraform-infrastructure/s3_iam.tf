114df08770112365cb397aa8e5bfd230c558bc95
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-20 13:49:11 +0100

    added permissions to s3 policy

--- terraform-infrastructure/s3_iam.tf
+++ terraform-infrastructure/s3_iam.tf
@@ -29,7 +29,7 @@ resource "aws_s3_bucket_policy" "example_policy" {
             # deny access to everyone except root user (or a role for a production setup)
             "aws:PrincipalArn" = [
               "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
-              "arn:aws:iam::${var.account_id}:role/devGitHubActionsProjectNameRole"
+              "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/devGitHubActionsProjectNameRole"
             ]
           }
         }
