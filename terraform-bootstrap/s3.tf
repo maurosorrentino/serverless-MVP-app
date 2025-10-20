8225387abe10ad8d8ab60c3c76d2e0133ac3d0e4
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-20 11:15:50 +0100

    added lambda s3 + prod env vars

--- terraform-bootstrap/s3.tf
+++ terraform-bootstrap/s3.tf
@@ -1,3 +1,7 @@
 resource "aws_s3_bucket" "terraform_state" {
-    bucket = "terraform-state-imperatives-department-name"
+    bucket = "terraform-state-department-name-testtt"
+}
+
+resource "aws_s3_bucket" "lambda_artifacts" {
+    bucket = "list-s3-contents-lambda-code-s3"
 }
