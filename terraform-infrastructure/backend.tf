663e7e75262c65a56b67e3b0ae02d6aeac3b8d17
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-20 13:58:23 +0100

    tf fmt

--- terraform-infrastructure/backend.tf
+++ terraform-infrastructure/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     # bucket, key, dynamo table and region is in the terraform init command and 
     # env variable are in .github/workflows/ENV/dev or prod/env_vars.json
-    encrypt        = true
+    encrypt = true
   }
 }
--- terraform-infrastructure/kms.tf
+++ terraform-infrastructure/kms.tf
@@ -1,8 +1,8 @@
 module "project_name_s3_kms" {
   source = "./modules/s3_kms"
 
-  account_id       = data.aws_caller_identity.current.id
-  bucket_name      = local.project_name_contents_s3_name
+  account_id               = data.aws_caller_identity.current.id
+  bucket_name              = local.project_name_contents_s3_name
   github_actions_role_name = local.github_actions_role_name
 
   depends_on = [module.project_name_lambda]
