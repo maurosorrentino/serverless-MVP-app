07106297adb4bb6e6b0b825c5c15f496dba48049
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-10-22 15:05:40 +0100

    replaced env json file with yaml env vars, needs testing

--- terraform-infrastructure/locals.tf
+++ terraform-infrastructure/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  project_name_contents_s3_name = "${var.project_name}-contents-${var.AWS_REGION}-imperatives-s3"
+  project_name_contents_s3_name = "${var.project_name}-contents-${var.region}-imperatives-s3"
   github_actions_role_name      = "devGitHubActionsProjectNameRole"
   lambda_role_name              = "${var.project_name}NameLambdaRole"
   api_name = "${var.project_name}-api-gateway"
