8acd761b8dc88ef1232054acbbc97e3e56e4a107
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-11-02 17:17:32 +0000

    added step for loading env vars from json to deal with more than 1 pipeline

--- terraform-infrastructure/backend.tf
+++ terraform-infrastructure/backend.tf
@@ -3,5 +3,9 @@ terraform {
     # bucket, key, dynamo table and region is in the terraform init command and 
     # env variable are in pipeline
     encrypt = true
+    bucket         = "terraform-state-department-name"
+    key            = "terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "terraform-lock-table"
   }
 }
