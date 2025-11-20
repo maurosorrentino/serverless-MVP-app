8e9072efd1daa8f1882e056944c01cd155b73e9c
maurosorrentino <sorrentino.mauro95@gmail.com>
2025-11-20 18:12:07 +0000

    changed script to include filename

--- .github/scripts/build_artifact.sh
+++ .github/scripts/build_artifact.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-FILE_NAME="$1"
+ZIP_FILE_NAME="$1"
 VERSION="$2"
 LAMBDA_ARTIFACTS_BUCKET="$3"
+FILE_NAME="$3"
 
-S3_KEY="list-s3-contents-$VERSION-$FILE_NAME.zip"
+S3_KEY="list-s3-contents-$VERSION-$ZIP_FILE_NAME.zip"
 
 if aws s3 ls s3://$LAMBDA_ARTIFACTS_BUCKET/$S3_KEY; then
   echo "Artifact $S3_KEY already exists. Skipping build."
 else
   echo "Building package for version $VERSION"
   cd app
-  zip -r ../$FILE_NAME.zip .
+  zip -r ../$ZIP_FILE_NAME.zip $FILE_NAME
   cd ..
-  aws s3 cp $FILE_NAME.zip s3://$LAMBDA_ARTIFACTS_BUCKET/$S3_KEY
+  aws s3 cp $ZIP_FILE_NAME.zip s3://$LAMBDA_ARTIFACTS_BUCKET/$S3_KEY
 fi
